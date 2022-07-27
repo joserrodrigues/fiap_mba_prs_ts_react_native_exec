ebabc13fa2a1be903676e3440475482a9950b944
rubens Rodrigues <rubens@schoolguardian.app>
2022-07-27 00:33:37 -0300

    Class 1 - Exec 2 - React Hooks e React Native Elements

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { Text, View, TouchableOpacity } from "react-native";
+import styles from "./AppStyles";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  const addCount = () => {
+    setCount((count) => count + 1);
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.boxCounter}>
+        <Text style={styles.titleCounter}> Contador {count}</Text>
+      </View>
+
+      <TouchableOpacity onPress={addCount} style={styles.button}>
+        <Text style={styles.buttonText}> Adicionar</Text>
+      </TouchableOpacity>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
